465202e3e8e47071a9ec5b9a2d582ad0ae80552b
iuliaGrig <grigiulia787@outlook.com>
2024-06-20 23:15:13 -0400

    Update some js for responsiveness

--- create-a-website.js
+++ create-a-website.js
@@ -1,12 +1,21 @@
+window.addEventListener('resize', function() {
+    this.document.querySelector('main').style.marginTop = navHeight + 'px';
+});
+
 document.addEventListener('DOMContentLoaded', function() {
     const navHeight = document.querySelector('nav').offsetHeight;
     const mainContent = document.getElementById('main-content');
-    const videoHeader = document.querySelector('main header');
-
     mainContent.style.marginTop = navHeight + 'px';
 });
 
-window.addEventListener('resize', function() {
+document.addEventListener('DOMContentLoaded', function() {
     const navHeight = document.querySelector('nav').offsetHeight;
-    this.document.querySelector('main').style.marginTop = navHeight + 'px';
+    const mainContent = document.getElementById('main-content-creations');
+    mainContent.style.marginTop = navHeight + 'px';
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navHeight = document.querySelector('nav').offsetHeight;
+    const mainContent = document.getElementById('main-content-contact');
+    mainContent.style.marginTop = navHeight + 'px';
 })
